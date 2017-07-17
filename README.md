e6d4db6653aadf13987dd4e037ab2ae3b073b0c8
ben lamothe <zonotope@gmail.com>
2017-07-16 22:03:53 -0400

    fns to add, remove, and list shutdown hooks

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # shutdown
 
-A tiny library to manage [clojure](http://clojure.org) applications' shutdown
-hooks.
+A tiny library to manage a [clojure](http://clojure.org) application's
+jvm shutdown hooks. Inspired by [duct](https://github.com/duct-framework/duct)
 
 ## Usage
 
-FIXME
+```clojure
+(require '[shutdown.core :as shutdown])
+
+;; To add a 0-argument function to be executed before your app's runtime shuts
+;; down (normally):
+(shutdown/add-hook! ::descriptive-name #(println "It's about to go down!"))
+
+;; In case you later change your mind:
+(shutdown/remove-hook ::descriptive-name)
+```
 
 ## License
 
--- src/shutdown/core.clj
+++ src/shutdown/core.clj
@@ -1,6 +1,42 @@
 (ns shutdown.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce ^:private hooks (atom {}))
+
+(defn- run-hooks []
+  (doseq [hook (vals @hooks)]
+    (hook)))
+
+(defonce ^:private bind-hooks!
+  (delay (let [runtime (Runtime/getRuntime)]
+           (.addShutdownHook runtime (Thread. #'run-hooks))
+           :bound)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; public api                                                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn add-hook!
+  "Register a function to be called with no arguments when the
+  application shuts down. `f` must be a function of no arguments, and
+  `key` must be a unique."
+  [key f]
+  (force bind-hooks!)
+  (when (contains? @hooks key)
+    (throw (IllegalArgumentException. (str "Key " key " is already registered "
+                                           "to another shutdown hook."))))
+  (swap! hooks assoc key f)
+  true)
+
+(defn remove-hook!
+  "De-registers the shutdown hook associated with `key`"
+  [key]
+  (if (contains? @hooks key)
+    (let [hook (get @hooks key)]
+      (swap! hooks dissoc key)
+      true)
+    false))
+
+(defn registered-hooks
+  "Lists all the currently registered shutdown hooks"
+  []
+  (keys @hooks))
